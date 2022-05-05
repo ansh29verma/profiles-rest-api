81a42f7e580cf128c4e238e9d91aa8af0fc12c3d
Anshul <2000anshverma@gmail.com>
2022-05-06 01:11:52 +0530

    added userprofile feed api

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
-
+from django.conf import settings
 # Create your models here.
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
